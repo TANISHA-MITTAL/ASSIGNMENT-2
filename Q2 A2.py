65af6bb4f6e619cc487c5e816d11ad9549beb38f
TANISHA-MITTAL <121186664+TANISHA-MITTAL@users.noreply.github.com>
2022-12-22 12:59:21 +0530

    Add files via upload

---
+++ Q2 A2.py
@@ -0,0 +1,8 @@
+name = "TANISH JINDAL"
+sid = 22103116
+department_name = "Electronics and communication Engineering"
+cgpa = 9.9
+print("Hey," , name , "Here!")
+print("My SID is", sid )
+print("I am from" , department_name , "department and my CGPA is" , cgpa)
+
